ecacdf34ceccbd6be8cfbfa2205a3d7c33fb8b3c
Eman <emanaiman2211@gmail.com>
2021-01-20 13:19:32 +0500

    Push Notification

---
+++ app/src/main/java/com/example/ft/Member.java
@@ -0,0 +1,17 @@
+package com.example.ft;
+
+public class Member {
+
+    private String name;
+
+    public Member (){};
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
